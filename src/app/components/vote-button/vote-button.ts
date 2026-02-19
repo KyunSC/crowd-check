8d7fd1dedad95e4e268c83b777ae7a33c763c670
KyunSC <exsunnychen2006@gmail.com>
2026-02-19 10:49:20 -0500

    Added Vote buttons

---
+++ src/app/components/vote-button/vote-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vote-button',
+  imports: [],
+  templateUrl: './vote-button.html',
+  styleUrl: './vote-button.css',
+})
+export class VoteButton {
+
+}
--- src/app/pages/library-page/library-page.ts
+++ src/app/pages/library-page/library-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeButton } from '../../components/home-button/home-button';
+import { VoteButton } from '../../components/vote-button/vote-button';
 
 @Component({
   selector: 'app-library-page',
-  imports: [HomeButton],
+  imports: [HomeButton, VoteButton],
   templateUrl: './library-page.html',
   styleUrl: './library-page.css',
 })
