c4b3c2a8d0b8adfdb50d6fe3e1e6fa58880d6086
KyunSC <exsunnychen2006@gmail.com>
2026-02-19 10:49:20 -0500

    Added Vote buttons

--- src/app/pages/gym-page/gym-page.ts
+++ src/app/pages/gym-page/gym-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeButton } from '../../components/home-button/home-button';
+import { VoteButton } from '../../components/vote-button/vote-button';
 
 @Component({
   selector: 'app-gym-page',
-  imports: [HomeButton],
+  imports: [HomeButton, VoteButton],
   templateUrl: './gym-page.html',
   styleUrl: './gym-page.css',
 })
