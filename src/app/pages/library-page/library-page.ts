f849bb127fa9f15e5f9e8c82463d3d6c2679e029
KyunSC <exsunnychen2006@gmail.com>
2026-02-20 12:14:55 -0500

    Added crowdedness meter

--- src/app/pages/library-page/library-page.ts
+++ src/app/pages/library-page/library-page.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HomeButton } from '../../components/home-button/home-button';
 import { VoteButton } from '../../components/vote-button/vote-button';
+import { CrowdMeter } from '../../components/crowd-meter/crowd-meter';
 
 @Component({
   selector: 'app-library-page',
-  imports: [HomeButton, VoteButton],
+  imports: [HomeButton, VoteButton, CrowdMeter],
   templateUrl: './library-page.html',
   styleUrl: './library-page.css',
 })
 export class LibraryPage {
-
+  crowdLevel = signal(0);
 }
