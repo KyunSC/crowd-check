557132f9a3bb07de551c24e348d7e58e69f81727
KyunSC <exsunnychen2006@gmail.com>
2026-02-21 23:41:19 -0500

    Added confirm button

--- src/app/pages/library-page/library-page.ts
+++ src/app/pages/library-page/library-page.ts
@@ -2,13 +2,19 @@ import { Component, signal } from '@angular/core';
 import { HomeButton } from '../../components/home-button/home-button';
 import { VoteButton } from '../../components/vote-button/vote-button';
 import { CrowdMeter } from '../../components/crowd-meter/crowd-meter';
+import { ConfirmButton } from '../../components/confirm-button/confirm-button';
 
 @Component({
   selector: 'app-library-page',
-  imports: [HomeButton, VoteButton, CrowdMeter],
+  imports: [HomeButton, VoteButton, CrowdMeter, ConfirmButton],
   templateUrl: './library-page.html',
   styleUrl: './library-page.css',
 })
 export class LibraryPage {
   crowdLevel = signal(0);
+  pendingLevel = signal(0);
+
+  onConfirm() {
+    this.crowdLevel.set(this.pendingLevel());
+  }
 }
